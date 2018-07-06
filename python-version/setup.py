2edb89e5769889aa4e474239a07e0bd1799ff94b
shuoGG <328893769@qq.com>
2018-07-06 20:09:27 +0800

    add setup install_require

--- python-version/setup.py
+++ python-version/setup.py
@@ -2,7 +2,10 @@ from setuptools import setup
 
 setup(
     name="QCandyUi",        # 包名
-    version="0.1",              # 版本信息
+    version="0.1",          # 版本信息
     packages=['QCandyUi'],  # 要打包的项目文件夹
-    include_package_data=True
+    include_package_data=True,
+    install_requires= [
+        'pywin32'
+    ]
 )
