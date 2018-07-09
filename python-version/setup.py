4f4e9eedf36b939f039c917a3857a619cccd6cb5
shuoGG <328893769@qq.com>
2018-07-09 21:38:58 +0800

    pack to pypi

--- python-version/setup.py
+++ python-version/setup.py
@@ -1,10 +1,16 @@
 from setuptools import setup
 
 setup(
-    name="QCandyUi",        # 包名
-    version="0.1",          # 版本信息
-    packages=['QCandyUi'],  # 要打包的项目文件夹
+    name="QCandyUi",       
+    version="0.1",   
+    description="quick beautify your pyQt5 app",
+    keywords='python pyQt ui',
+    author='shuoGG',
+    author_email='328893769@qq.com',
+    url='https://github.com/shuoGG1239/QCandyUi',
+    packages=['QCandyUi'],
     include_package_data=True,
+    zip_safe=False,
     install_requires= [
         'pywin32'
     ]
