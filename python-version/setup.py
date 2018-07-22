68df3764671a5bbe49691032dbeacbf277d6294a
shuoGG <328893769@qq.com>
2018-07-22 21:04:57 +0800

    release ver 0.12

--- python-version/setup.py
+++ python-version/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="QCandyUi",       
-    version="0.11",   
+    version="0.12",   
     description="quick beautify your pyQt5 app",
     keywords='python pyQt ui',
     author='shuoGG',
